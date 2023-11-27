93b1104e23b704db8d5e88215c2b1b5ba68081b3
Guddu Ali <gudduali93@yahoo.com>
2023-11-27 16:27:05 +0530

    updated

---
+++ controllers/user.controllers.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const { User } = require("../model/user.model");
+const router = express.Router()
+
+router.get("/user", (req, res) => {
+  res.status(200).send("user fetched api updated successfully");
+});
+
+router.post("/user", (req, res) => {
+  const response = User();
+
+    res.status(200).json({
+      message: "User created successfully",
+      data: response,
+    });
+});
+
+router.put("/user/:id", (req, res) => {
+  if (req.params.id !== "123") {
+    return res.status(404).send("User not found");
+  }
+  res.status(200).send("User updated successfully");
+});
+
+router.delete("/user/:id", (req, res) => {
+  if (req.params.id !== "123") {
+    return res.status(404).send("User not found");
+  }
+  res.status(200).send("User delete successfully");
+});
+
+module.exports = router
--- index.js
+++ index.js
@@ -1,33 +1,14 @@
 require("dotenv/config");
 const express = require("express");
 const cors = require("cors");
+const router = require("./controllers/user.controllers");
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json()); 
+app.use(express.json());
 app.use(cors());
 
-app.get("/user", (req, res) => {
-  res.status(200).send("user fetched api updated successfully");
-});
-
-app.post("/user", (req, res) => {
-  res.status(200).json({ message:"User created successfully", data: req.body });
-});
-
-app.put("/user/:id", (req, res) => {
-  if (req.params.id !== "123") {
-    return res.status(404).send("User not found");
-  }
-  res.status(200).send("User updated successfully");
-});
-
-app.delete("/user/:id", (req, res) => {
-  if(req.params.id !== "123"){
-    return res.status(404).send("User not found");
-  }
-  res.status(200).send("User delete successfully");
-});
+app.use("/", router);
 
 const PORT = process.env.PORT || 8080;
 const server = app.listen(PORT, async () => {
